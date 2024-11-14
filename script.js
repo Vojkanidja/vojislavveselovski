6fe994ae10c4aaff34a0599810430c607ac192e2
Vojkanidja <vojo05@gmail.com>
2024-11-14 20:06:56 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+const menuicon=document.querySelector('.menu-icon')
+const container=document.querySelector('.container')
+
+menuicon.addEventListener('click',()=>{
+container.classList.toggle('navigate')
+})
